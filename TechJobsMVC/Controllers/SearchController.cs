c4856cd901cfcb8bc7453ae0ffd4256ac07536e5
Courtney Anderson <iamcourtcourt@yahoo.com>
2020-12-01 17:25:17 -0600

    Finihsed Assignment #3

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,13 +12,50 @@ namespace TechJobsMVC.Controllers
 {
     public class SearchController : Controller
     {
+
+
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+            //ViewBag.columns = ColumnChoices;
+
+            ViewBag.tableChoices = ListController.TableChoices;
+            ViewBag.employers = JobData.GetAllEmployers();
+            ViewBag.locations = JobData.GetAllLocations();
+            ViewBag.positionTypes = JobData.GetAllPositionTypes();
+            ViewBag.skills = JobData.GetAllCoreCompetencies();
+
             return View();
+
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.searchtype = searchType;
+            ViewBag.searchTerm = searchTerm;
+            List<Job> jobs;
+            if (searchTerm == null)
+            {
+
+                jobs = JobData.FindAll();
+                ViewBag.title = "All Jobs";
+            }
+
+            else
+            {
+                ViewBag.searchtype = searchType;
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Your Search Catrgoty: " + searchType.ToUpper() + " and Search Term: " + searchTerm.ToUpper();
+            }
+
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.ColumnChoices;
+            return View("Index");
+        }
     }
 }
